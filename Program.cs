c7078a2a3541af5294e7e18f9626994987b2ec94
jiujiu5201314 <226023085@qq.com>
2026-05-30 23:07:49 +0800

    完成TODO列表周末项目：添加+勾选+删除

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TodoForm());
+            Application.Run(new Form1());
         }
     }
 }
--- TodoForm.cs
+++ TodoForm.cs
@@ -6,26 +6,57 @@ namespace WindowsFormsApp1
 {
     public class TodoForm : Form
     {
+        private TextBox tbx = new TextBox();
+        private Button but = new Button();
+        private CheckedListBox clb = new CheckedListBox();
+        private Button btnDel = new Button();
+
+
         public TodoForm()
         {
             this.Text = "TODO 列表";
             this.ClientSize = new Size(450, 420);
 
-            TextBox tbx = new TextBox();
+            //输入框
             tbx.Location = new Point(20, 20);
             tbx.Size = new Size(300, 25);
-            this.Controls.Add (tbx);
+            this.Controls.Add(tbx);
 
-            Button but = new Button();
+            //添加按钮
             but.Text = "添加";
             but.Location = new Point(330, 18);
             but.Size = new Size(80, 30);
-            this.Controls.Add (but);
+            this.Controls.Add(but);
+            but.Click += But_Click;
 
-            CheckedListBox clb = new CheckedListBox();
+            //代办列表
             clb.Location = new Point(20, 60);
             clb.Size = new Size(390, 300);
-            this.Controls.Add (clb);
+            this.Controls.Add(clb);
+
+            btnDel.Text = "删除已完成";
+            btnDel.Location = new Point(20, 370);
+            btnDel.Size = new Size(120, 30);
+            btnDel.Click += BtnDel_Click;
+            this.Controls.Add (btnDel);
+
+
+        }
+
+        private void But_Click(object sender, EventArgs e)
+        {
+            clb.Items.Add(tbx.Text);
+            tbx.Clear();
+        }
+        private void BtnDel_Click(object sender,EventArgs e)
+        {
+            for (int i = clb.Items.Count - 1; i >= 0; i--)
+            {
+                if (clb.GetItemChecked(i))
+                {
+                    clb.Items.RemoveAt(i);
+                }
+            }
         }
     }
 }
